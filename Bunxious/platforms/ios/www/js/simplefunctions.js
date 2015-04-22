c16e848e78a41b2a97ac9a6746247e79bd5c4159
Ricardo Torres <ricardotorres0727@gmail.com>
2015-04-22 06:31:40 +0200

    Slide Bar

---
+++ Bunxious/platforms/ios/www/js/simplefunctions.js
@@ -0,0 +1,4 @@
+function includeHTML(container_id, load_html)
+{
+	$('#'+container_id).load(load_html);
+}
---
+++ Bunxious/www/js/toggle.js
@@ -0,0 +1,32 @@
+var toggle_shown = false;
+
+function toggle_sidebar(show)
+{
+    var sidebar = document.getElementById("sidebar");
+    var toggle = document.getElementById("toggle");
+    var mainview = document.getElementById("mainview");
+           
+//            console.log(sidebar.style.left);
+           
+    if(show)
+    {
+        sidebar.style.left = "0px";
+        mainview.style.left="100px";
+        toggle.style.left="210px";
+        toggle_shown=true;
+    }
+    else
+    {
+        sidebar.style.left = "-200px";
+        mainview.style.left="0px";
+        toggle.style.left="10px";
+        toggle_shown=false;
+    }
+}
+
+function onclick_togglebutton(){
+	if(toggle_shown)
+		toggle_sidebar(false);
+	else
+		toggle_sidebar(true);
+}

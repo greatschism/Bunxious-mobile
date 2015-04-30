dd190a85332f5b06335dffd966b7e8e2519c6b97
adlai_griffith <macsolve@gmail.com>
2015-04-29 23:15:15 -0500

    fix iframe scrolling

--- Bunxious/platforms/ios/Bunxious/Classes/AppDelegate.m
+++ Bunxious/platforms/ios/Bunxious/Classes/AppDelegate.m
@@ -80,8 +80,9 @@
 
     // Set your app's start page by setting the <content src='foo.html' /> tag in config.xml.
     // If necessary, uncomment the line below to override it.
-    // self.viewController.startPage = @"index.html";
-
+     self.viewController.startPage = @"index.html";
+ //   self.viewController.wwwFolderName = @"";
+ //   self.viewController.startPage = @"http://www.bunxious.com/";
     // NOTE: To customize the view's frame size (which defaults to full screen), override
     // [self.viewController viewWillAppear:] in your view controller.
 

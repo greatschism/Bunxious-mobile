d9dfe5e676490aace95d09ea62f0833c8dd89d46
Lego <legolas8911@gmail.com>
2015-05-10 15:43:58 +0300

    fixed typo

--- app/controllers/product/productRow.js
+++ app/controllers/product/productRow.js
@@ -4,5 +4,5 @@ $.pinImage.image = args.image_big.image;
 $.avatar.image = args.user.avatar_medium.image;
 $.title.text = args.title;
 $.description.text = args.description.replace('\r', '').replace('\n', ''); //stripping new lines
-$.price.text = Alloy.Global.Utils.codeToCurrency(args.currency_code) + args.price;
+$.price.text = Alloy.Globals.Utils.codeToCurrency(args.currency_code) + args.price;
 

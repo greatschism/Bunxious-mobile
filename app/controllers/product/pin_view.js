becda9ecd24d097e52e5598fc8c67349b92825d0
mide765 <mide.smile@gmail.com>
2015-05-21 15:59:06 +0300

    Finished Layout for the Pin Page

--- app/controllers/product/pin_view.js
+++ app/controllers/product/pin_view.js
@@ -2,12 +2,9 @@ var args = arguments[0] || {};
 
 Ti.API.info('args: ' + JSON.stringify(args));
 
-
 $.pinImage.image = args.image_big.image;
 $.title.text = args.title;
 $.price.text = Alloy.Globals.Utils.codeToCurrency(args.currency_code) + args.price + ' ' + args.currency_code.toUpperCase();
-
-
 
 $.brand.text = 'BRAND: ' + args.brand_id;
 $.size.text = 'SIZE: Not yet defined';
@@ -15,14 +12,23 @@ $.condition.text = 'CONDITION: ' + args.condition_id;
 $.description.value = 'Description: ' + args.description + ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mattis lectus. Nunc et mattis sapien. Praesent ac metus ac dolor laoreet vulputate ac eu lacus. Donec accumsan ornare ex, ac luctus tortor finibus vehicula. Nunc quis hendrerit eros, eu ultrices velit. Aliquam erat volutpat. Ut vestibulum in elit ut vestibulum. ';
 $.shipping.text = '    SHIPPING FROM ' + args.from;
 
-
 $.shipToOut.text = 'United States';
 $.shipCostOut.text = '$6.50 USD';
 $.shipWithOut.text = '$1.00 USD';
 
+Alloy.Globals.API.getUser(args.user_id, function(result) {
 
+	$.avatar.image = result.avatar_medium.image;
+	$.userName.text = result.firstname + ' ' + result.lastname;
 
+	if (result.city && result.country_iso_code_3) {
 
+		$.userLocation.text = result.city + ', ' + result.country_iso_code_3;
+	}
+	else if (result.city || result.country_iso_code_3) {
 
+		$.userLocation.text = result.city || result.country_iso_code_3;
+	}
 
+});
 

c76714557ef53eb2bc2fb11d22d4eff9958a309c
Mihai Blaga <legolas8911@gmail.com>
2015-06-15 23:20:31 +0300

    Cannot Register new account through app , Spacing is off -  “unsigned” is meshing with Date joined, Description not matching with live site, Link to user does not work done

--- app/controllers/profile/user_view.js
+++ app/controllers/profile/user_view.js
@@ -8,3 +8,10 @@ $.gender.text = args.gender ? args.gender : '';
 
 var date = moment(args.date_added);
 $.joined.text = 'Joined ' + date.format("MMMM D, YYYY");
+
+$.avatar.addEventListener('click', function() {
+
+	Alloy.Globals.openWindow('profile/profile_view', {
+		user_id : args.id
+	}, true);
+}); 

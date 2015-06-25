4c0f5560d11bc0f7ca40febfe1bf06c2b7071428
SPorwal <shraddhaporwal@gmail.com>
2015-06-25 14:44:36 +0530

    Open pin-view from closet

--- app/controllers/product/closetRow.js
+++ app/controllers/product/closetRow.js
@@ -5,3 +5,13 @@ $.avatar.image = args.pin.avatar;
 $.title.text = args.pin.title;
 $.description.text = args.pin.description;
 $.price.text = String.formatCurrency(args.pin.price);
+
+
+$.pinImage.addEventListener('click', function(e) {
+	// console.debug('closet row product/pin_view args ', JSON.stringify(args));
+	
+	var data = args.pin;
+	data.image_big = args.img;
+	
+	Alloy.Globals.openWindow('product/pin_view', data, true);
+}); 

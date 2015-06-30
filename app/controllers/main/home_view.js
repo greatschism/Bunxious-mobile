dfb8834dd255392e3a568f61eb4885edc8f4438c
SPorwal <shraddhaporwal@gmail.com>
2015-06-30 18:21:42 +0530

    Filter Service calls and results on home screen added

--- app/controllers/main/home_view.js
+++ app/controllers/main/home_view.js
@@ -1,4 +1,5 @@
 var args = arguments[0] || {};
+var filters = {};
 
 function populateTable() {
 
@@ -56,7 +57,21 @@ if (!args.fromAnotherController) {
 	}
 }
 
-function createFilter(list, label){
+function getIDByItem(list, item) {
+	var id;
+	
+	for (i in list){
+		if(list[i].title === item){
+			console.debug("list[i] ", JSON.stringify(list[i]));
+			id = list[i].id;
+			break;
+		}
+	}
+	
+	return id;
+}
+
+function createFilter(list, label, filterType){
 	
 	var items = [];
 	
@@ -74,6 +89,59 @@ function createFilter(list, label){
 
 			label.text = e.row.data.title;
 			popupDialog.hide();
+			
+			// Update filters []
+			var keyArray = [];
+			
+			// Getting array of keys
+			for (var key in filters) {
+				// console.log("key ", key); // shows key
+				keyArray.push(key);
+			}
+			
+			// Checking the filter type
+			if(filterType === "category"){
+				
+				filters['filters[category_id]'] = getIDByItem(list, e.row.data.title);
+				
+			} else if(filterType === "brand"){
+
+				filters['filters[brand_id]'] = getIDByItem(list, e.row.data.title);
+				
+			} else if(filterType === "gender"){
+				
+				filters['filters[gender_id]'] = getIDByItem(list, e.row.data.title);
+				
+			} else if(filterType === "size"){
+				
+				filters['filters[size_id]'] = getIDByItem(list, e.row.data.title);
+				
+			}
+			
+			// Call the service
+			Alloy.Globals.loading.show();
+			var productArray = [];
+			Alloy.Globals.API.getFilteredPins(filters, function(results) {
+		
+				console.debug("Alloy.Globals.API.getFilteredPins", JSON.stringify(results));
+				
+				for (var i in results) {
+					productArray.push(Alloy.createController('product/productRow', results[i]).getView());
+				}
+		
+				if (results.length == 0) {
+					productArray.push(Ti.UI.createTableViewRow({
+						title : 'No results.'
+					}));
+				}
+				$.homeTable.setData(productArray);
+				Alloy.Globals.loading.hide();
+				
+			}, function(error) {
+
+				Alloy.Globals.loading.hide();
+			});
+			
 		});
 
 		popupDialog.getView().show();
@@ -83,14 +151,14 @@ $.categoryFilter.addEventListener('click', function() {
 	
 	if(Alloy.Globals.categoryFilters) {
 		
-		createFilter(Alloy.Globals.categoryFilters, $.categoryLabel);
+		createFilter(Alloy.Globals.categoryFilters, $.categoryLabel, "category");
 		
 	} else {
 
 		Alloy.Globals.API.getAllCategories(function(results) {
 			
 			Alloy.Globals.categoryFilters = JSON.parse(JSON.stringify(results));
-			createFilter(Alloy.Globals.categoryFilters, $.categoryLabel);
+			createFilter(Alloy.Globals.categoryFilters, $.categoryLabel, "category");
 	
 		}, function(error) {
 	
@@ -124,14 +192,14 @@ $.brandFilter.addEventListener('click', function() {
 	
 	if(Alloy.Globals.brandFilters) {
 
-		createFilter(Alloy.Globals.brandFilters, $.brandLabel);
+		createFilter(Alloy.Globals.brandFilters, $.brandLabel, "brand");
 
 	} else {
 		Alloy.Globals.API.getBrands(function(results) {
 			
 			Alloy.Globals.brandFilters = JSON.parse(JSON.stringify(results));
 
-			createFilter(Alloy.Globals.brandFilters, $.brandLabel);
+			createFilter(Alloy.Globals.brandFilters, $.brandLabel, "brand");
 	
 		}, function(error) {
 	
@@ -143,7 +211,7 @@ $.genderFilter.addEventListener('click', function() {
 	
 	if(Alloy.Globals.genderFilters) {
 
-		createFilter(Alloy.Globals.genderFilters, $.genderLabel);
+		createFilter(Alloy.Globals.genderFilters, $.genderLabel, "gender");
 
 	} else {
 		Alloy.Globals.API.getGender(function(results) {
@@ -156,7 +224,7 @@ $.genderFilter.addEventListener('click', function() {
 				});
 			}
 			Alloy.Globals.genderFilters = JSON.parse(JSON.stringify(items));
-			createFilter(Alloy.Globals.genderFilters, $.genderLabel);
+			createFilter(Alloy.Globals.genderFilters, $.genderLabel, "gender");
 	
 		}, function(error) {
 	
@@ -168,13 +236,13 @@ $.sizeFilter.addEventListener('click', function() {
 	
 	if(Alloy.Globals.sizeFilters) {
 
-		createFilter(Alloy.Globals.sizeFilters, $.sizeLabel);
+		createFilter(Alloy.Globals.sizeFilters, $.sizeLabel, "size");
 
 	} else {
 		Alloy.Globals.API.getSize(function(results) {
 			
 			Alloy.Globals.sizeFilters = JSON.parse(JSON.stringify(results));
-			createFilter(Alloy.Globals.sizeFilters, $.sizeLabel);
+			createFilter(Alloy.Globals.sizeFilters, $.sizeLabel, "size");
 	
 		}, function(error) {
 	

43c950ea10d273499f2a9ff39eda0bff41681d24
SPorwal <shraddhaporwal@gmail.com>
2015-07-13 22:09:50 +0530

    Set default currency to USD

--- app/controllers/product/pin_view.js
+++ app/controllers/product/pin_view.js
@@ -10,7 +10,12 @@ function displayPin() {
 
 	$.pinImage.image = args.image_big.image;
 	$.title.text = args.title;
-	$.price.text = Alloy.Globals.Utils.codeToCurrency(args.currency_code) + args.price + ' ' + args.currency_code.toUpperCase();
+	
+	if (args.currency_code){
+		$.price.text = Alloy.Globals.Utils.codeToCurrency(args.currency_code) + args.price + ' ' + args.currency_code.toUpperCase();
+	} else {
+		$.price.text = '$' + args.price + ' ' + 'USD';
+	}
 
 	$.brand.text = 'BRAND: ' + args.brand_id;
 	$.size.text = 'SIZE: Not yet defined';

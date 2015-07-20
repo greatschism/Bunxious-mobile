a35c9fb2a997386af8af8cd65d5b90257a78e26b
Sagar Patel <saggy_6161@ymail.com>
2015-07-20 22:43:47 +0530

    1) Implementing the search items in  my group tab

--- app/controllers/profile/group_view.js
+++ app/controllers/profile/group_view.js
@@ -2,6 +2,7 @@ var args = arguments[0] || {};
 var moment = require('alloy/moment');
 var group_id = args.group.id;
 var group_name = args.group.name;
+var filters = {};
 
 $.title.addOnReturn(function(event) {
 
@@ -58,10 +59,6 @@ for (var i in args.posts.Post) {
 	tableData.push(Alloy.createController('profile/groupPostRow', args.posts.Post[i]).getView());
 }
 
-$.search.addEventListener('return', function(term){
-	// Perform group view search
-});
-
 $.editGroup.addEventListener('click', function() {
 	Alloy.Globals.openWindow('profile/createGroup_view', { update : true, id : args.group.id, name : args.group.name, description : args.group.description}, true);
 });
@@ -115,3 +112,161 @@ Ti.App.addEventListener("updateGroup_groupView",function(data){
 
 $.postsTable.setData(tableData);
 
+//================== Group items Filter==============
+
+$.categoryFilter.addEventListener('click', function() {
+
+	if (Alloy.Globals.categoryFilters) {
+
+		createFilter(Alloy.Globals.categoryFilters, $.categoryLabel, "category");
+
+	} else {
+
+		Alloy.Globals.API.getAllCategories(function(results) {
+
+			Alloy.Globals.categoryFilters = JSON.parse(JSON.stringify(results));
+			createFilter(Alloy.Globals.categoryFilters, $.categoryLabel, "category");
+
+		}, function(error) {
+
+		});
+	}
+});
+
+$.brandFilter.addEventListener('click', function() {
+
+	if (Alloy.Globals.brandFilters) {
+
+		createFilter(Alloy.Globals.brandFilters, $.brandLabel, "brand");
+
+	} else {
+		Alloy.Globals.API.getBrands(function(results) {
+
+			Alloy.Globals.brandFilters = JSON.parse(JSON.stringify(results));
+
+			createFilter(Alloy.Globals.brandFilters, $.brandLabel, "brand");
+
+		}, function(error) {
+
+		});
+	}
+});
+
+$.genderFilter.addEventListener('click', function() {
+
+	if (Alloy.Globals.genderFilters) {
+
+		createFilter(Alloy.Globals.genderFilters, $.genderLabel, "gender");
+
+	} else {
+		Alloy.Globals.API.getGender(function(results) {
+
+			var items = [];
+			for (var i in results.Gender) {
+				items.push({
+					"title" : results.Gender[i].name,
+					"id" : results.Gender[i].id
+				});
+			}
+			Alloy.Globals.genderFilters = JSON.parse(JSON.stringify(items));
+			createFilter(Alloy.Globals.genderFilters, $.genderLabel, "gender");
+
+		}, function(error) {
+
+		});
+	}
+});
+
+$.sizeFilter.addEventListener('click', function() {
+
+	if (Alloy.Globals.sizeFilters) {
+
+		createFilter(Alloy.Globals.sizeFilters, $.sizeLabel, "size");
+
+	} else {
+		Alloy.Globals.API.getSize(function(results) {
+
+			Alloy.Globals.sizeFilters = JSON.parse(JSON.stringify(results));
+			createFilter(Alloy.Globals.sizeFilters, $.sizeLabel, "size");
+
+		}, function(error) {
+
+		});
+	}
+});
+
+function createFilter(list, label, filterType) {
+
+	var items = [];
+
+	for (i in list) {
+		items.push(list[i].title);
+	}
+
+	var popupDialog = Alloy.createWidget('ti.ux.popup.list', 'widget', {
+		closeButton : true,
+		selectable : true,
+		options : items,
+	});
+
+	popupDialog.getView('table').addEventListener('click', function(e) {
+
+		label.text = e.row.data.title;
+		popupDialog.hide();
+
+		// Update filters []
+		var keyArray = [];
+
+		// Getting array of keys
+		for (var key in filters) {
+			// console.log("key ", key); // shows key
+			keyArray.push(key);
+		}
+
+		// Checking the filter type
+		if (filterType === "category") {
+
+			filters['filters[category_id]'] = Alloy.Globals.getIDByItem(list, e.row.data.title);
+
+		} else if (filterType === "brand") {
+
+			filters['filters[brand_id]'] = Alloy.Globals.getIDByItem(list, e.row.data.title);
+
+		} else if (filterType === "gender") {
+
+			filters['filters[gender_id]'] = Alloy.Globals.getIDByItem(list, e.row.data.title);
+
+		} else if (filterType === "size") {
+
+			filters['filters[size_id]'] = Alloy.Globals.getIDByItem(list, e.row.data.title);
+
+		}
+
+		// Call the service
+		Alloy.Globals.loading.show();
+		var productArray = [];
+		Alloy.Globals.API.getFilteredPins(filters, function(results) {
+
+			console.debug("Alloy.Globals.API.getFilteredPins", JSON.stringify(results));
+
+			for (var i in results) {
+				productArray.push(Alloy.createController('product/productRow', results[i]).getView());
+			}
+
+			if (results.length == 0) {
+				productArray.push(Ti.UI.createTableViewRow({
+					title : 'No results.'
+				}));
+			}
+			$.homeTable.setData(productArray);
+			Alloy.Globals.loading.hide();
+
+		}, function(error) {
+
+			Alloy.Globals.loading.hide();
+		});
+
+	});
+
+	popupDialog.getView().show();
+}

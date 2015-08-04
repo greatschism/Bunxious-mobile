18ae96daabe4e43f4e1620bd9eaf2608abc155ea
SPorwal <shraddhaporwal@gmail.com>
2015-08-04 15:32:20 +0530

    Removed Styled label and added an image check

--- app/controllers/profile/groupPostRow.js
+++ app/controllers/profile/groupPostRow.js
@@ -11,11 +11,15 @@ if(args.pin_id) {
 } else {
 	// No Pin image
 	$.post_title.show();
-	$.pin_img.image = args.article.Image;
-	// $.pin_img.hide();
-	// $.pin_img.setHeight(0);
+	if(args.article.Image){
+		$.pin_img.image = args.article.Image;
+	} else {
+		$.pin_img.hide();
+		$.pin_img.setHeight(0);
+	}
 	// Adding html style as it is a styled label, unable to set text
-	$.post_title.html = '<style>a, span {font-family : HelveticaNeue; word-wrap : break-word;}</style><span>' + args.article.title + '</span>';
+	// $.post_title.html = '<style>a, span {font-family : HelveticaNeue; word-wrap : break-word;}</style><span>' + args.article.title + '</span>';
+	$.post_title.text = args.article.title.trim();
 }
 
 $.title.addOnReturn(function(event) {

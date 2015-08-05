f0ac023ecf6ad73e62ae8fd62c6004ce88cfb072
Mihai Blaga <legolas8911@gmail.com>
2015-08-06 00:53:09 +0300

    issue #120, changed the env to NONE, please have a look now. On my end it works after the login

--- app/controllers/cart/order.js
+++ app/controllers/cart/order.js
@@ -68,10 +68,10 @@ function addButtonToWindow() {
 		textStyle : Paypal.PAYPAL_TEXT_PAY, // Causes the button's text to change from "Pay" to "Donate"
 
 		appID : 'APP-80W284485P519543T', // The appID issued by Paypal for your application; for testing, feel free to delete this property entirely.
-		paypalEnvironment : Paypal.PAYPAL_ENV_SANDBOX, // Sandbox, None or Live
+		paypalEnvironment : Paypal.PAYPAL_ENV_NONE, // Sandbox, None or Live
 
 		feePaidByReceiver : false,
-		//enableShipping : false, // Whether or not to select/send shipping information
+		enableShipping : false, // Whether or not to select/send shipping information
 
 		payment : {// The payment itself
 			paymentType : Paypal.PAYMENT_TYPE_GOODS, // The type of payment

b42e40bc709df9ac2d71e443f915daf3b6ef5f25
Eric Duvon <eric@chie.do>
2015-08-06 16:35:01 -0400

    user search functionality

--- app/controllers/misc/search.js
+++ app/controllers/misc/search.js
@@ -1,3 +1,19 @@
 var args = arguments[0] || {};
 
-alert(args.searchTerm);
+Alloy.Globals.API.searchUsers(args.searchTerm, function(results){
+
+	console.debug("user-search-result ", JSON.stringify(results));
+
+	var tableData = [];
+
+		for (var i in results) {
+
+			//Use user list view 
+			tableData.push(Alloy.createController('profile/user_view', results[i]).getView());
+		}
+
+		$.searchTable.setData(tableData);
+
+}, function(error){
+
+});

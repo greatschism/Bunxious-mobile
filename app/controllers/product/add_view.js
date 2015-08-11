67c5123562abe6ea638857b5868060698c42eb23
Mihai Blaga <legolas8911@gmail.com>
2015-08-12 02:46:19 +0300

    closes #134

--- app/controllers/product/add_view.js
+++ app/controllers/product/add_view.js
@@ -354,7 +354,6 @@ function setDefaults() {
 	$.tfStyle.setValue("");
 	$.tfTags.setValue("");
 	$.tfMaterials.setValue("");
-	$.tfProcessingTime.setValue("");
 	$.shipToUS.setValue("");
 	$.shipToElsewhere.setValue("");
 
@@ -394,7 +393,7 @@ $.addItem.addEventListener('click', function() {
 	}
 
 	// Checking if all fields are filled
-	if ($.tfTitle.getValue().trim() === "" || $.tfDesc.getValue().trim() === "" || $.categoryTitle.idValue === "" || $.brandTitle.idValue === "" || $.genderTitle.idValue === "" || $.conditionTitle.idValue === "" || $.tfStyle.getValue().trim() === "" || $.tfMaterials.getValue() === "" || $.tfProcessingTime.getValue().trim() === "" || $.shipToUS.getValue().trim() === "" || $.shipToElsewhere.getValue().trim() === "") {
+	if ($.tfTitle.getValue().trim() === "" || $.tfDesc.getValue().trim() === "" || $.categoryTitle.idValue === "" || $.brandTitle.idValue === "" || $.genderTitle.idValue === "" || $.conditionTitle.idValue === "" || $.tfStyle.getValue().trim() === "" || $.tfMaterials.getValue() === "" || $.shipToUS.getValue().trim() === "" || $.shipToElsewhere.getValue().trim() === "") {
 		alert(L("missing_field_error"));
 		return;
 	}

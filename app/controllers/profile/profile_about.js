2a76d598217e955bbf6f66c492c7394d46ac66b7
Mihai Blaga <legolas8911@gmail.com>
2015-09-02 01:19:45 +0300

    closes #217

--- app/controllers/profile/profile_about.js
+++ app/controllers/profile/profile_about.js
@@ -3,8 +3,28 @@ var args = arguments[0] || {};
 $.aboutText.text = Alloy.Globals.Utils.Encoder.htmlDecode(args.about);
 $.favLabel.text = args.firstname + "'s Closet";
 
-$.favWrapper.addEventListener('click', function(e){
+$.favWrapper.addEventListener('click', function(e) {
 	Alloy.Globals.openWindow('profile/closet_view', {
 		user_id : args.id
 	}, true);
+});
+
+Alloy.Globals.API.getCloset(args.id, function(result) {
+
+	for (i in result.MyPins.pinImg) {
+
+		$.pins.add(Alloy.createController('product/pinview_closet_item_image', {
+			image : result.MyPins.pinImg[i].image,
+			pinId : result.MyPins.pin[i].id,
+			user_id : args.id,
+			callback : function(callbackPin) {
+
+				Alloy.Globals.openWindow('product/pin_view', callbackPin, true);
+			}
+		}).getView());
+	}
+
+}, function(error) {
+
+	//TBD
 });
